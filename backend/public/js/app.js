dcc36010ab210683c526b8ca1cbcc7159f673259
RajikuBlanc <rajiku.blanc@gmail.com>
2021-06-16 00:22:01 +0900

    comp

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -1870,9 +1870,15 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+
+
 function Example() {
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("h1", {
-    children: "Hello React!"
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.Fragment, {
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("h1", {
+      children: "Hello React!"
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("h2", {
+      children: "test"
+    })]
   });
 }
 
--- backend/resources/js/components/Example.js
+++ backend/resources/js/components/Example.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Example() {
-  return <h1>Hello React!</h1>;
+  return (
+    <>
+      <h1>Hello React!</h1>
+      <h2>test</h2>
+    </>
+  );
 }
 
 if (document.getElementById('root')) {
